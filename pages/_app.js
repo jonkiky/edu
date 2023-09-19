0175e72727caed494bacd20ec137a5a0a0584e99
Chen <jonkiky@gmail.com>
2023-09-19 00:22:26 -0400

    update the home page

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,9 @@ const App = ({ Component, pageProps }) => {
        <meta charSet="utf-8" />
        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900" />
+       <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Georama:wght@500&family=Nunito:wght@200&family=Roboto&display=swap" rel="stylesheet" />
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       <div className="wrapper">
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,80 @@
 import { useState } from 'react';
 import Image from 'next/image'; 
+import Link from 'next/link';
 
 export default function Home() {
 
   return (
   	  <div id="home">
 
-					<div  className="home-image">
-						<Image src="/sport.png"  alt="background Image" layout="fill"
-        objectFit="cover"/>
+			<section id="head">
+				<div id="image"><Image
+					  src="/images/bruce-mars-xj8qrWvuOEs-unsplash.jpg"
+					  alt="Description of the image"
+						layout="fill"
+					  objectFit="cover"
+					/>
 					</div>
-					<div class="home-paper">
-					 	<div id="logo"><h2>Explore</h2></div>
-						<div id="slogan">
-							<p> Learn and Discover Endless Adventures for Your Little Ones.</p>
-						</div>
-						<div id="explore-button">
-						<a href="/explore" class="button primary submit-btn">Explore</a>
-						</div>
+				<div id="head-text">
+					<div id="logo">	<Link href="/explore">Explore</Link></div>
+					<div id="slogan">
+						<div id="slogan1">Looking for Kids' activities</div>
+						<div id="slogan2">Feeling exhausted</div>
+						<div id="slogan3">Let's handle the <span className="trend-list">research</span> for you.</div>
+					</div>
+					<div class="explore-btn">
+									<Link href="/explore">
+									  Explore
+									</Link></div>
+					</div>
+			</section>
+
+			<section id="question">
+				
+				<h1>The Question We Answer</h1>
+				<div id="question-list">
+					<div className="q">
+							<div className="q-h">What</div>
+							<div className="q-d">What kids can do</div>
+					</div>
+
+					<div className="q">
+							<div className="q-h">When</div>
+							<div className="q-d">When kids can start it</div>
 					</div>
 					
+						<div className="q">
+							<div className="q-h">Why</div>
+							<div className="q-d">Why is it good for your kids</div>
+					</div>
 
-	</div>
+						<div className="q">
+							<div className="q-h">Cost</div>
+							<div className="q-d">maximum your edu ROI</div>
+					</div>
+				</div>
+			</section>
+
+				<section id="solution">
+					<div id="solution-list">
+						<ul>
+							<li>Lists activities kids can play</li>
+							<li>Facets search on age, mulit-intellignet, season, cost</li>
+							<li>Details report on an active which covers all you want to know</li>
+						</ul>
+					</div>
+					<div class="explore-btn">
+								<Link href="/explore">
+								  Explore
+								</Link></div>
+				</section>
+				<section >
+					<div><h1>Checkout what we have..</h1></div>
+					<div class="explore-btn">
+								<Link href="/explore">
+								  Explore
+								</Link></div>
+				</section>
+		</div>
   );
 }
