d96d8677760b89ab70d604302f06bc6f495d3b1f
Chen <jonkiky@gmail.com>
2023-10-29 01:27:39 -0400

    update img

--- pages/use.js
+++ pages/use.js
@@ -1,31 +1,16 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
 import { InfinitySpin } from 'react-loader-spinner';
+import Header from "../components/header";
 import Link from 'next/link';
 
 
 
 export default function page() {
-
 	return (
-
 	<div id="search-page">
 			<div className="search-paper">
-				 	<header>
-						 	<div id="search-logo"> 
-						 		<Link href="/">
-									<span className="symbol">
-										<img src="images/logo.png" alt="" />
-									</span>
-								</Link>
-
-						 	<Link href="/">
-								   <h2>Kid Education Hub</h2>
-								</Link>
-						 	</div>
-						 	<div class="line"></div>
-		       </header>
-
+				 	<Header />
 		      <div className="inner" id="content-sections">
 								"to be done"
 					</div>
