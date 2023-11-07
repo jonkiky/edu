25679f495aa73699fe9813ea822b636766cb1f9a
Chen <jonkiky@gmail.com>
2023-11-06 23:09:19 -0500

    add few activites

--- config/search_cate.js
+++ config/search_cate.js
@@ -46,7 +46,7 @@ export const data = [
         "tags": [
             "age-9",  
             "s-spring", "s-summer", "s-fall", "s-winter",
-            "c-$",
+            "c-$$",
                  "mi-methematical" ],
         "cate": "mi-methematical"
     },
@@ -55,7 +55,7 @@ export const data = [
         "desc": "",
         "tags": [
             "age-6", "age-7", "age-8", "age-9",
-                 "s-summer", "s-fall", "c-$",
+                 "s-summer", "s-fall", "c-$$",
                   "mi-bodily-kinesthetic","mi-interpersonal" ],
         "cate": "mi-bodily-kinesthetic"
     },
@@ -116,9 +116,87 @@ export const data = [
            "age-5","age-6","age-7", "age-8", "age-9", 
            "s-spring", "s-summer", "s-fall","s-winter",
            "c-$$",
-             "mi-musical"],
+             "mi-bodily-kinesthetic","mi-intrapersonal"],
         "cate": "mi-musical"
     },
-
-
+    {
+        "name": "Basketball",
+        "desc": "",
+        "tags": [
+           "age-5","age-6","age-7", "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$",
+             "mi-bodily-kinesthetic","mi-interpersonal"],
+        "cate": "mi-bodily-kinesthetic"
+    },
+    {
+        "name": "Beach Volleyball",
+        "desc": "",
+        "tags": [
+           "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$",
+             "mi-bodily-kinesthetic","mi-interpersonal"],
+        "cate": "mi-bodily-kinesthetic"
+    },
+     {
+        "name": "Bead Art",
+        "desc": "",
+        "tags": [
+          "age-4","age-5","age-6","age-7", "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$",
+             "mi-visual"],
+        "cate":  "mi-visual"
+    },
+    {
+        "name": "Beauty Pageant",
+        "desc": "",
+        "tags": [
+          "age-4","age-5","age-6","age-7", "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$$$",
+             "mi-intrapersonal","mi-interpersonal",],
+        "cate":  "mi-intrapersonal"
+    },
+    {
+        "name": "Biathlon",
+        "desc": "",
+        "tags": [
+         "age-8", "age-9", 
+           "s-winter",
+           "c-$$$",
+             "mi-bodily-kinesthetic"],
+        "cate":  "mi-bodily-kinesthetic"
+    },
+    {
+        "name": "Biology",
+        "desc": "",
+        "tags": [
+           "age-4","age-5","age-6","age-7", "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$",
+             "mi-interpersonal"],
+        "cate":  "mi-interpersonal"
+    },
+    {
+        "name": "Bobsleigh",
+        "desc": "",
+        "tags": [
+          "age-9", 
+           "s-winter",
+           "c-$$$",
+            "mi-bodily-kinesthetic"],
+        "cate":  "mi-bodily-kinesthetic"
+    },
+     {
+        "name": "Boxing",
+        "desc": "",
+        "tags": [
+          "age-8", "age-9", 
+           "s-spring", "s-summer", "s-fall","s-winter",
+           "c-$$",
+            "mi-bodily-kinesthetic"],
+        "cate":  "mi-bodily-kinesthetic"
+    },
 ]
